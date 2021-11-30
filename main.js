43ff961f900a332564d2e976982630bfbcb0f827
Breanne <BreMarotta@gmail.com>
2021-11-30 11:51:09 -0500

    Created JavaScript to meet requirements

--- main.js
+++ main.js
@@ -2,8 +2,46 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// My JavaScript Starts here::
+const hearts = document.querySelectorAll('.like-glyph');
+  //console.log(hearts);
+  for(const heart of hearts){
+    heart.addEventListener("click", handleClick)}
 
+
+
+
+//Functions
+function handleClick(e){
+  mimicServerCall()
+  .then(() => {
+    handleLike(e);
+  })
+  .catch(() => {
+    handleError()
+  })
+}
+//----------------------------------------------------------------------
+//Build function to handle like-- needs to change heart to filled in heart
+function handleLike(e){
+  const heart = e.target;
+  if (heart.innerText === EMPTY_HEART) {
+  heart.innerText = FULL_HEART;
+  heart.className = "activated-heart";
+  } else {
+  heart.innerText = EMPTY_HEART;
+  heart.className = "";
+};
+}
+//----------------------------------------------------------------------
+//Build function to handle server error
+function handleError(){
+  console.log("Got to my ERROR function")
+  const modal = document.querySelector("div#modal");
+  modal.className = "";
+  modal.innerText = "Random server error. Try again.";
+  setTimeout(foo => modal.className = "hidden",
+   3000)};
 
 
 
